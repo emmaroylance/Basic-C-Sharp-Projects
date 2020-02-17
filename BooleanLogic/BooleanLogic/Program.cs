a2184aac9326abdf3a49213b3260069c5122842c
emmaroylance <emmers646@gmail.com>
2020-02-17 14:23:16 -0700

    Boolean Logic

---
+++ BooleanLogic/BooleanLogic/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(true && true && true || false); = true
+            //Console.WriteLine(true && false);
+            //Console.WriteLine(true && true);
+            //Console.WriteLine(false && false);
+
+            //Console.WriteLine(true || false);
+            //Console.WriteLine(true || true);
+            //Console.WriteLine(false || false);
+
+            //Console.WriteLine(true == false);
+            //Console.WriteLine(true == true);
+            //Console.WriteLine(false == false);
+
+            //Console.WriteLine(true != false);
+            //Console.WriteLine(true != true);
+            //Console.WriteLine(false != false);
+
+            // ^ operator will evaluate to true when it is OR and only OR, not And. (XOR operator)
+            //Console.WriteLine(true ^ false);
+            //Console.WriteLine(true ^ true);
+            //Console.WriteLine(false ^ false);
+
+
+            Console.ReadLine();
+        }
+    }
+}
