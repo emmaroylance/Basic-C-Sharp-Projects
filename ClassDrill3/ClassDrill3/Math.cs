b06bc3a5b009a1e84074a0d1c3c2090580c8242e
emmaroylance <emmers646@gmail.com>
2020-02-19 19:16:38 -0700

    ClassDrill3 Problems

---
+++ ClassDrill3/ClassDrill3/Math.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDrill3
+{
+    class Math
+    {
+        public int Operate(int userValue, userValue2)
+        {
+            return userValue + 5 + userValue2;
+        }
+    }
+}
---
+++ ClassDrill3/ClassDrill3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDrill3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math operatorObject = new Math();
+            int userValue2;
+            int result;
+            Console.WriteLine("Pick a number:");
+            int userValue = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Pick another number--optional");
+            if (int.TryParse(Console.ReadLine(), out userValue2))
+            {
+                result = operatorObject.Operate(userValue, userValue2);
+            }
+            else
+            {
+               result =  Math.Operate(userValue);
+            }
+        }
+    }
+}
