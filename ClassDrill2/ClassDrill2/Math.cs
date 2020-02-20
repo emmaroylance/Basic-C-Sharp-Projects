3a2e2f58e5ddce77979502eda1dabd576a4f7b8c
emmaroylance <emmers646@gmail.com>
2020-02-19 18:28:04 -0700

    ClassDrill2

---
+++ ClassDrill2/ClassDrill2/Math.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDrill2
+{
+    class Math
+    {
+        public int Operations(int userInput1)
+        {
+            int addition = userInput1 + 10;
+            Console.WriteLine(userInput1 + " plus 10 equals " + addition);
+            return addition;
+        }
+
+        public double Operations(double userInput2)
+        {
+            double product = userInput2 * 30;
+            Console.WriteLine(userInput2 + " multiplied by 30 equals " + product);
+            return product;
+        }
+
+        public int Operations(string userInput3)
+        {
+            int integer = Convert.ToInt32(userInput3);
+            int quotient = integer / 2;
+            Console.WriteLine(integer + " divided by 2 equals " + quotient);
+            return quotient;
+        }
+    }
+}
---
+++ ClassDrill2/ClassDrill2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDrill2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math numbers = new Math();
+            Console.WriteLine("Please type a number: ");
+            int userInput1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please type a number with a decimal: ");
+            double userInput2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Please type a number: ");
+            string userInput3 = Console.ReadLine();
+            Console.ReadLine();
+
+            numbers.Operations(userInput1);
+            numbers.Operations(userInput2);
+            numbers.Operations(userInput3);
+        }
+        
+    }
+}
