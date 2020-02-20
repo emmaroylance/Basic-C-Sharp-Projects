732da8bae900a40af4dd319084ee75fa027a0b85
emmaroylance <emmers646@gmail.com>
2020-02-20 15:35:22 -0700

    Inheritance Drill

---
+++ InheritanceDrill/InheritanceDrill/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceDrill
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public void SayName(string FirstName, string LastName)
+        {
+            Console.WriteLine("Name: " + FirstName + " " + LastName );
+        }
+    }
+}
---
+++ InheritanceDrill/InheritanceDrill/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee Person1 = new Employee();
+            Person1.FirstName = "Sample";
+            Person1.LastName = "Student";
+            Person1.SayName(Person1.FirstName, Person1.LastName);  
+        }
+    }
+}
