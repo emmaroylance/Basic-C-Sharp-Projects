8ae5542bec38b4f1fb8be5ae8ca8d00c6e8a6c94
emmaroylance <emmers646@gmail.com>
2020-02-20 12:59:25 -0700

    Class Drill 3 Update

--- ClassDrill3/ClassDrill3/Math.cs
+++ ClassDrill3/ClassDrill3/Math.cs
@@ -8,9 +8,16 @@ namespace ClassDrill3
 {
     class Math
     {
-        public int Operate(int userValue, userValue2)
+        //public int Operate(int userValue, userValue2)
+        //{
+        //    return userValue + 5 + userValue2;
+        //}
+
+        public int Operate(int userValue, int userValue2int = 0)
         {
-            return userValue + 5 + userValue2;
+            int addition = userValue + userValue2int + 4;
+            Console.WriteLine(userValue + userValue2int + " plus 4 equals " + addition);
+            return addition;
         }
     }
 }
--- ClassDrill3/ClassDrill3/Program.cs
+++ ClassDrill3/ClassDrill3/Program.cs
@@ -10,21 +10,41 @@ namespace ClassDrill3
     {
         static void Main(string[] args)
         {
-            Math operatorObject = new Math();
-            int userValue2;
-            int result;
-            Console.WriteLine("Pick a number:");
+            Math numbers = new Math();
+            Console.WriteLine("Please type a number: ");
             int userValue = Convert.ToInt32(Console.ReadLine());
-
+            //Console.WriteLine("Please type a number--optional:");
+            //int userValue2 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Pick another number--optional");
-            if (int.TryParse(Console.ReadLine(), out userValue2))
+            string userValue2 = Console.ReadLine();
+            if (userValue2.Length > 0)
             {
-                result = operatorObject.Operate(userValue, userValue2);
+                int userValue2int = Convert.ToInt32(userValue2);
+                numbers.Operate(userValue, userValue2int);
             }
             else
-            {
-               result =  Math.Operate(userValue);
+            { 
+                numbers.Operate(userValue);
             }
         }
+
+        //static void Main(string[] args)
+        //{
+        //    Math operatorObject = new Math();
+        //    int userValue2;
+        //    int result;
+        //    Console.WriteLine("Pick a number:");
+        //    int userValue = Convert.ToInt32(Console.ReadLine());
+
+        //    Console.WriteLine("Pick another number--optional");
+        //    if (int.TryParse(Console.ReadLine(), out userValue2))
+        //    {
+        //        result = operatorObject.Operate(userValue, userValue2);
+        //    }
+        //    else
+        //    {
+        //       result =  Math.Operate(userValue);
+        //    }
+        //}
     }
 }
