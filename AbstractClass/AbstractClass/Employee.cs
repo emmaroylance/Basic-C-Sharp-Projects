e2883b0bf26e9841ae41d658599e45d79354eb91
emmaroylance <emmers646@gmail.com>
2020-02-21 18:18:20 -0700

    Generic Lists

--- AbstractClass/AbstractClass/Employee.cs
+++ AbstractClass/AbstractClass/Employee.cs
@@ -6,11 +6,13 @@ using System.Threading.Tasks;
 
 namespace AbstractClass
 {
-    public class Employee : Person, IQuittable
+    public class Employee<T> : Person, IQuittable
     {
         public int Id { get; set; }
         public bool IsActive { get; set; }
         public bool IsEqual { get; set; }
+
+        public List<T> things { get; set; }
 
         public Employee()
         {
@@ -28,14 +30,14 @@ namespace AbstractClass
             IsActive = false;
         }
 
-        public static bool operator== (Employee Person1, Employee Person2)
-        {
-            return Person1.Id.Equals(Person2.Id);       
-        }
+        //public static bool operator== (Employee Person1, Employee Person2)
+        //{
+        //    return Person1.Id.Equals(Person2.Id);       
+        //}
 
-        public static bool operator!= (Employee Person1, Employee Person2)
-        {
-            return Person1.Id.Equals(Person2.Id);
-        }
+        //public static bool operator!= (Employee Person1, Employee Person2)
+        //{
+        //    return Person1.Id.Equals(Person2.Id);
+        //}
     }
 }
--- AbstractClass/AbstractClass/Program.cs
+++ AbstractClass/AbstractClass/Program.cs
@@ -10,25 +10,44 @@ namespace AbstractClass
     {
         static void Main(string[] args)
         {
-            Employee Person1 = new Employee();
-            Employee Person2 = new Employee();
-            Employee isequals = new Employee();
-            Person1.FirstName = "Sample";
-            Person1.LastName = "Student";
-            Person1.Id = 1;
-            Person2.FirstName = "John";
-            Person2.LastName = "Smith";
-            Person2.Id = 2;
+
+            Employee<string> stringsOfThings = new Employee<string>();
+            Employee<int> intsOfThings = new Employee<int>();
+            stringsOfThings.things = new List<string>() {"DOB", "Address", "Phone Number", "Years Employed"};
+            intsOfThings.things = new List<int>() {1, 2, 3, 4, 5};
+  
+
+            for (int j = 0; j < stringsOfThings.things.Count; j++)
+            {
+                Console.WriteLine(stringsOfThings.things[j]);
+            }
+
+            for (int i = 0; i < intsOfThings.things.Count; i++)
+            {
+                Console.WriteLine(intsOfThings.things[i]);
+            }
+
+            Console.ReadLine();
+
+            //Employee Person1 = new Employee();
+            //Employee Person2 = new Employee();
+            //Employee isequals = new Employee();
+            //Person1.FirstName = "Sample";
+            //Person1.LastName = "Student";
+            //Person1.Id = 1;
+            //Person2.FirstName = "John";
+            //Person2.LastName = "Smith";
+            //Person2.Id = 2;
 
 
-            Person1.SayName();
-            Console.ReadLine();
-            IQuittable employee = new Employee();
-            Console.WriteLine(Person1.FirstName + " " + Person1.LastName +  ": " + employee.IsActive);
-            employee.Quit();
-            Console.WriteLine(Person1.FirstName + " " + Person1.LastName + ": " + employee.IsActive);
-            Console.WriteLine(Person1 == Person2);
-            Console.ReadLine();
+            //Person1.SayName();
+            //Console.ReadLine();
+            //IQuittable employee = new Employee();
+            //Console.WriteLine(Person1.FirstName + " " + Person1.LastName +  ": " + employee.IsActive);
+            //employee.Quit();
+            //Console.WriteLine(Person1.FirstName + " " + Person1.LastName + ": " + employee.IsActive);
+            //Console.WriteLine(Person1 == Person2);
+            //Console.ReadLine();
         }
     }
 }
