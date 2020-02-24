fbc244d9e55fec62207f677be01b09ddc7693d82
emmaroylance <emmers646@gmail.com>
2020-02-24 15:12:02 -0700

    Structs Drill

---
+++ StructsDrill/StructsDrill/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructsDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number1 = new Number();
+            number1.Amount = 2.5;
+            Console.WriteLine(number1.Amount);
+            Console.ReadLine();
+        }
+    }
+}
