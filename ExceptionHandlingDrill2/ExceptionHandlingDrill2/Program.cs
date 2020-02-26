369d5d35123120afa1cda7c4d8adaaa41cb352d6
emmaroylance <emmers646@gmail.com>
2020-02-26 16:38:04 -0700

    Exception Handling Drill

---
+++ ExceptionHandlingDrill2/ExceptionHandlingDrill2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandlingDrill2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your age: ");
+            try
+            {
+                int userAge = Convert.ToInt32(Console.ReadLine());
+                if (userAge <= 0)
+                {
+                    Console.WriteLine("Your value must be more than zero");
+                    Console.ReadLine();
+                    return;
+                }
+                else
+                {
+                    int yearBorn = 2020 - userAge;
+                    Console.WriteLine("You were born in {0}", yearBorn);
+                    Console.ReadLine();
+                }
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("You entered something incorrectly");
+                Console.ReadLine();
+            }
+
+
+        }
+    }
+}
