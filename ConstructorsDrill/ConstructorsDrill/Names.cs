026a017994715dc40a390c34c6c69c60561212e1
emmaroylance <emmers646@gmail.com>
2020-02-26 15:43:09 -0700

    ConstructorsDrill

---
+++ ConstructorsDrill/ConstructorsDrill/Names.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorsDrill
+{
+    public class Names
+    {
+        public string lastName { get; set; }
+        public string firstName { get; set; }
+
+        public void People(string firstNameOfPeople, string lastNameOfPeople)
+        {
+            lastName = lastNameOfPeople;
+            firstName = firstNameOfPeople;
+            Console.WriteLine(firstName + " " + lastName);
+        }
+
+        public void People(string firstNameOfPeople) : this(firstNameOfPeople, "Smith" )
+        {
+        }
+
+    }
+}
---
+++ ConstructorsDrill/ConstructorsDrill/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorsDrill
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            const string name = "Emma";
+            var listOfNames = new List<string>() { name, "Tyler", "Kent", "Karalee" };
+
+
+        Console.WriteLine("Please enter your first name: ");
+        string firstNameOfPeople = Console.ReadLine();
+
+        Console.WriteLine("Please enter your last name: ");
+        string lastNameOfPeople = Console.ReadLine();
+
+        var fullName1 = new Names.People("Emma");
+        Console.WriteLine(fullName1);
+
+
+        }
+    }
+}
